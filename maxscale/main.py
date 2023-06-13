b5eb7c80133eb85bc807a1fef5735f071de0bd37
Ceceskii <97866655+Ceceskii@users.noreply.github.com>
2023-06-12 17:58:39 -0700

    update main

--- maxscale/main.py
+++ maxscale/main.py
@@ -1,10 +1,15 @@
-import pymysql
+# Celine Watcharaapakorn-Smith
+# CNE 370
+# 06.12.2023
+# Database Sharding: To utilize horizontal sharding and Docker to develop a scalable and effective database solution.
 
-db = pymysql.connect(host="127.0.0.1", port=4000, user="maxuser", passwd="maxpwd")
+import mysql.connector
+
+db = mysql.connector.connect(host="172.21.0.4", port="4000", user="maxuser", password="maxpwd")
 cursor = db.cursor()
 
 print('The last 10 rows of zipcodes_one are:')
-cursor.execute("SELECT * FROM zipcodes_one.zipcodes_one LIMIT 9990,10;")
+cursor.execute("SELECT * FROM zipcodes_one.zipcodes_one LIMIT 10;")
 results = cursor.fetchall()
 for result in results:
     print(result)

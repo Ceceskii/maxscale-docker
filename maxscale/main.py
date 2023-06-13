6de952de6f1772307674b988182328ce54a154ad
Ceceskii <97866655+Ceceskii@users.noreply.github.com>
2023-06-13 16:57:40 -0700

    Update main.py

--- maxscale/main.py
+++ maxscale/main.py
@@ -8,28 +8,36 @@ import mysql.connector
 db = mysql.connector.connect(host="172.21.0.4", port="4000", user="maxuser", password="maxpwd")
 cursor = db.cursor()
 
-print('The last 10 rows of zipcodes_one are:')
-cursor.execute("SELECT * FROM zipcodes_one.zipcodes_one LIMIT 10;")
+
+print('The largest zipcode in zipcodes_one:')
+cursor.execute("SELECT Zipcode FROM zipcodes_one ORDER BY Zipcode DESC LIMIT 1;")
 results = cursor.fetchall()
 for result in results:
     print(result)
 
-print('The first 10 rows of zipcodes_two are:')
-cursor.execute("SELECT * FROM zipcodes_two.zipcodes_two LIMIT 10")
+
+
+print('All zipcodes where state = KY:')
+cursor.execute("SELECT Zipcode FROM zipcodes_one WHERE State = 'KY' UNION SELECT Zipcode FROM zipcodes_two WHERE State = 'KY';")
 results = cursor.fetchall()
 for result in results:
     print(result)
 
-print('The largest zipcode number in zipcodes_one is:')
+
+
+print('All zipcodes between 40000 and 41000:')
 cursor = db.cursor()
-cursor.execute("SELECT Zipcode FROM zipcodes_one.zipcodes_one ORDER BY Zipcode DESC LIMIT 1;")
+cursor.execute("SELECT Zipcode FROM zipcodes_one WHERE zipcode BETWEEN 40000 AND 41000 UNION SELECT Zipcode FROM zipcodes_two WHERE zipcode BETWEEN 40000 AND 41000;")
 results = cursor.fetchall()
 for result in results:
     print(result)
 
-print('The smallest zipcode number in zipcodes_two is:')
+
+
+print('The TotalWages column where state = PA:')
 cursor = db.cursor()
-cursor.execute("SELECT Zipcode FROM zipcodes_two.zipcodes_two ORDER BY Zipcode ASC LIMIT 1;")
+cursor.execute("SELECT TotalWages FROM zipcodes_one WHERE state = 'PA' UNION ALL SELECT TotalWages FROM zipcodes_two WHERE state = 'PA';")
 results = cursor.fetchall()
 for result in results:
     print(result)
+

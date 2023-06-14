4334ed2aa5e1743d65a2842bbecc217fdb68d704
Ceceskii <97866655+Ceceskii@users.noreply.github.com>
2023-06-13 18:12:04 -0700

    Update main.py

--- maxscale/main.py
+++ maxscale/main.py
@@ -10,34 +10,50 @@ cursor = db.cursor()
 
 
 print('The largest zipcode in zipcodes_one:')
-cursor.execute("SELECT Zipcode FROM zipcodes_one.zipcodes_one_one ORDER BY Zipcode DESC LIMIT 1;")
+cursor.execute("SELECT Zipcode FROM zipcodes_one.zipcodes_one ORDER BY Zipcode DESC LIMIT 1;")
 results = cursor.fetchall()
 for result in results:
     print(result)
-
 
 
 print('All zipcodes where state = KY:')
-cursor.execute("SELECT Zipcode FROM zipcodes_one.zipcodes_one WHERE State = 'KY' UNION SELECT Zipcode FROM zipcodes_two.zipcodes_two WHERE State = 'KY';")
+cursor.execute("SELECT Zipcode FROM zipcodes_one.zipcodes_one WHERE State = 'KY';")
 results = cursor.fetchall()
 for result in results:
-    print(result)
-
+        if result[0] != "":
+                print(result)
+cursor.execute("SELECT Zipcode FROM zipcodes_two.zipcodes_two WHERE State = 'KY';") 
+results = cursor.fetchall()
+for result in results:
+        if result[0] != "":
+                print(result)
 
 
 print('All zipcodes between 40000 and 41000:')
 cursor = db.cursor()
-cursor.execute("SELECT Zipcode FROM zipcodes_one.zipcodes_one WHERE zipcode BETWEEN 40000 AND 41000 UNION SELECT Zipcode FROM zipcodes_two.zipcodes_two WHERE zipcode BETWEEN 40000 AND 41000;")
+cursor.execute("SELECT Zipcode FROM zipcodes_one.zipcodes_one WHERE zipcode BETWEEN 40000 AND 41000;")
 results = cursor.fetchall()
 for result in results:
-    print(result)
-
-
+        if result[0] != "":
+                print(result) 
+cursor.execute("SELECT Zipcode FROM zipcodes_two.zipcodes_two WHERE zipcode BETWEEN 40000 AND 41000;")
+results = cursor.fetchall()
+for result in results:
+        if result[0] != "":
+                print(result)
 
 print('The TotalWages column where state = PA:')
 cursor = db.cursor()
-cursor.execute("SELECT TotalWages FROM zipcodes_one.zipcodes_one WHERE state = 'PA' UNION ALL SELECT TotalWages FROM zipcodes_two.zipcodes_two WHERE state = 'PA';")
+cursor.execute("SELECT TotalWages FROM zipcodes_one.zipcodes_one WHERE state = 'PA';")
 results = cursor.fetchall()
 for result in results:
-    print(result)
+        if result[0] != "":
+                print(result)
+cursor.execute("SELECT ALL TotalWages FROM zipcodes_two.zipcodes_two WHERE state = 'PA';")
+results = cursor.fetchall()
+for result in results:
+        if result[0] != "":
+                print(result)
+
+
 
